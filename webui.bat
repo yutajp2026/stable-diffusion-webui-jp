9d31d06081db076df1953e3290ecb61b999c3ae4
yuta <y.takak1234@gmail.com>
2026-08-23 09:06:27 +0900

    changed

--- webui.bat
+++ webui.bat
@@ -1,8 +1,6 @@
 @echo off
 
-if exist webui.settings.bat (
-    call webui.settings.bat
-)
+title Stable Diffusion WebUI - Python と pip を確認しています...
 
 if not defined PYTHON (set PYTHON=python)
 if defined GIT (set "GIT_PYTHON_GIT_EXECUTABLE=%GIT%")
@@ -15,8 +13,10 @@ mkdir tmp 2>NUL
 
 %PYTHON% -c "" >tmp/stdout.txt 2>tmp/stderr.txt
 if %ERRORLEVEL% == 0 goto :check_pip
-echo Couldn't launch python
-goto :show_stdout_stderr
+title Stable Diffusion WebUI - Pythonをインストールしています...
+curl  -L -O "https://www.python.org/ftp/python/3.10.6/python-3.10.6-amd64.exe"
+echo msgbox "Pythonをインストールします。「Add python.exe to PATH」にチェックを入れ、「Install Now」を選択してください。インストールできたら、このウィンドウを閉じてください。" > %TEMP%/msgboxtest.vbs & %TEMP%/msgboxtest.vbs
+start python-3.10.6-amd64.exe & goto :show_stdout_stderr
 
 :check_pip
 %PYTHON% -mpip --help >tmp/stdout.txt 2>tmp/stderr.txt
@@ -24,54 +24,52 @@ if %ERRORLEVEL% == 0 goto :start_venv
 if "%PIP_INSTALLER_LOCATION%" == "" goto :show_stdout_stderr
 %PYTHON% "%PIP_INSTALLER_LOCATION%" >tmp/stdout.txt 2>tmp/stderr.txt
 if %ERRORLEVEL% == 0 goto :start_venv
-echo Couldn't install pip
 goto :show_stdout_stderr
 
 :start_venv
 if ["%VENV_DIR%"] == ["-"] goto :skip_venv
 if ["%SKIP_VENV%"] == ["1"] goto :skip_venv
 
+
 dir "%VENV_DIR%\Scripts\Python.exe" >tmp/stdout.txt 2>tmp/stderr.txt
 if %ERRORLEVEL% == 0 goto :activate_venv
 
 for /f "delims=" %%i in ('CALL %PYTHON% -c "import sys; print(sys.executable)"') do set PYTHON_FULLNAME="%%i"
-echo Creating venv in directory %VENV_DIR% using python %PYTHON_FULLNAME%
+title Stable Diffusion WebUI - %PYTHON_FULLNAME% で仮想環境 %VENV_DIR% を作成しています...
 %PYTHON_FULLNAME% -m venv "%VENV_DIR%" >tmp/stdout.txt 2>tmp/stderr.txt
 if %ERRORLEVEL% == 0 goto :upgrade_pip
-echo Unable to create venv in directory "%VENV_DIR%"
 goto :show_stdout_stderr
 
 :upgrade_pip
+title Stable Diffusion WebUI - PIPをアップグレードしています...
 "%VENV_DIR%\Scripts\Python.exe" -m pip install --upgrade pip
 if %ERRORLEVEL% == 0 goto :activate_venv
-echo Warning: Failed to upgrade PIP version
+echo 警告: PIP をアップグレードできませんでした。
 
 :activate_venv
+title Stable Diffusion WebUI - %VENV_DIR%\Scripts\activate.bat
 set PYTHON="%VENV_DIR%\Scripts\Python.exe"
 call "%VENV_DIR%\Scripts\activate.bat"
-echo venv %PYTHON%
 
 :skip_venv
-if [%ACCELERATE%] == ["True"] goto :accelerate
+if "%ACCELERATE%" == "True" goto :accelerate
 goto :launch
 
 :accelerate
-echo Checking for accelerate
 set ACCELERATE="%VENV_DIR%\Scripts\accelerate.exe"
 if EXIST %ACCELERATE% goto :accelerate_launch
 
 :launch
+title Stable Diffusion WebUI %COMMANDLINE_ARGS%
 %PYTHON% launch.py %*
 if EXIST tmp/restart goto :skip_venv
-pause
-exit /b
+goto :endofscript
 
 :accelerate_launch
-echo Accelerating
+title Stable Diffusion WebUI %COMMANDLINE_ARGS% with %ACCELERATE%
 %ACCELERATE% launch --num_cpu_threads_per_process=6 launch.py
 if EXIST tmp/restart goto :skip_venv
-pause
-exit /b
+goto :endofscript
 
 :show_stdout_stderr
 
@@ -94,5 +92,4 @@ type tmp\stderr.txt
 :endofscript
 
 echo.
-echo Launch unsuccessful. Exiting.
-pause
+echo msgbox "起動に失敗しました。" > %TEMP%/msgboxtest.vbs & %TEMP%/msgboxtest.vbs
